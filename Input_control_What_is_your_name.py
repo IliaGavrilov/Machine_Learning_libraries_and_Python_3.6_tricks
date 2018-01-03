6d242b524b45c1260f07db48d272c28a6f3cf131
Ilia Gavrilov <elijahgavri@gmail.com>
2018-01-03 15:14:59 +0300

    Create Input_control_What_is_your_name.py

---
+++ Input_control_What_is_your_name.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 12 11:15:59 2017
+@author: Gavrilov
+a small game for my nephew
+"""
+x = input("What's your name? ")
+for i in x:
+    if i=="S" or i=="s" and i=="a" and i=="h": #if i=="S" or i=="s" and i=="a" and i=="h":
+        print("Cool")
+        break
+else:
+    print("Anyways")
